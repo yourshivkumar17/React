9b29c02ab161f5ed33b7ff14f869cef00a94e00e
Shiv Kumar <yourshivkumar17@gmail.com>
2026-07-05 15:00:58 +0530

    Update Rendering Lists (map())8.jsx

--- Rendering Lists (map())8.jsx
+++ Rendering Lists (map())8.jsx
@@ -20,3 +20,46 @@ Kya tum aise likhoge? ❌
 Agar 100 students ho gaye to? 😅
 
 Isliye React me map() use karte hain.
+  --------------------------------------
+  React me map() use karte waqt key dena hota hai.
+  
+  import "./App.css";
+
+function App() {
+  const students = [
+    {
+      id: 1,
+      name: "Shiv Kumar",
+      course: "B.Tech CSE",
+      city: "Agra",
+    },
+    {
+      id: 2,
+      name: "Rahul Kumar",
+      course: "BCA",
+      city: "Delhi",
+    },
+    {
+      id: 3,
+      name: "Aman Singh",
+      course: "MCA",
+      city: "Jaipur",
+    },
+  ];
+
+  return (
+    <div className="container">
+      <h1>Student List</h1>
+
+      {students.map((student) => (
+        <div className="card" key={student.id}>
+          <h2>{student.name}</h2>
+          <p>Course : {student.course}</p>
+          <p>City : {student.city}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
