d506eab7692ae4c370c0d8fb7ee40b9f10959782
Shiv Kumar <yourshivkumar17@gmail.com>
2026-07-05 14:57:44 +0530

    Create Rendering Lists (map())8.jsx

---
+++ Rendering Lists (map())8.jsx
@@ -0,0 +1,22 @@
+📘 Chapter 7 : Rendering Lists (map())
+Problem
+
+Maan lo tumhare paas 5 students hain.
+
+Shiv
+Rahul
+Aman
+Rohit
+Amit
+
+Kya tum aise likhoge? ❌
+
+<h2>Shiv</h2>
+<h2>Rahul</h2>
+<h2>Aman</h2>
+<h2>Rohit</h2>
+<h2>Amit</h2>
+
+Agar 100 students ho gaye to? 😅
+
+Isliye React me map() use karte hain.
